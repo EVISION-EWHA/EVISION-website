440b7f1d73de98ebbeaadf690ac01375873faeca
soo <tnrud7844@ewhain.net>
2023-02-06 16:24:32 +0900

    글 목록 생성 전

---
+++ src/pages/board/post.jsx
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+import './import.css';
+
+function Post() {
+  return (
+    <div className="Post">
+      <h1>게시판</h1>
+      <div className='form-wrapper'>
+        <h3>제목
+        <input className="title-input" type='text'/>
+        </h3>
+        <h4>내용</h4>
+          <textarea className="text-area" type='text'></textarea>
+      </div>
+      <button className="submit-button">작성하기</button>
+    </div>
+  );
+}
+
+export default Post;
