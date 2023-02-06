cbb3999afeb1d05fd4d6e3be7be837d62b21bf53
soo <tnrud7844@ewhain.net>
2023-02-06 20:17:47 +0900

    error 있음....

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "pages/login/Login";
 import Signup from "pages/sign/Signup";
+import Post from "pages/board/post";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/login" element={<Login></Login>} />
       <Route path="/signup" element={<Signup></Signup>} />
+      <Route path="/post" element={<Post></Post>} />
       <Route path="/" element={<Login></Login>} />
     </Routes>
   );
