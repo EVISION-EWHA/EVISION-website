1c470aabf7bd71b825f738e48319047c935a3dab
chlqls <chlqls212@gmail.com>
2023-02-08 01:53:57 +0900

    230208 메인페이지 폴페이지 적용

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "pages/login/Login";
 import Signup from "pages/sign/Signup";
+import MainPage from "pages/Main/MainPage";
+import CurriculumPage from "pages/Curriculum/CurriculumPage";
+
+import Header from "components/header";
+import Footer from "components/footer";
 
 const App = () => {
   return (
-    <Routes>
-      <Route path="/login" element={<Login></Login>} />
-      <Route path="/signup" element={<Signup></Signup>} />
-      <Route path="/" element={<Login></Login>} />
-    </Routes>
+    <>
+      <Header />
+      <Routes>
+        <Route path="/login" element={<Login></Login>} />
+        <Route path="/signup" element={<Signup></Signup>} />
+        <Route path="/" element={<Login></Login>} />
+        <Route path="/main" element={<MainPage></MainPage>} />
+        <Route path="/curriculum" element={<CurriculumPage></CurriculumPage>} />
+      </Routes>
+      <Footer />
+    </>
   );
 };
 
--- src/pages/Curriculum/CurriculumPage.jsx
+++ src/pages/Curriculum/CurriculumPage.jsx
@@ -1,15 +1,5 @@
-import Curriculum from "components/curri";
-import { StLanding } from "pages/Main/MainPage";
-import Fade from "react-reveal/Fade";
-
 const CurriculumPage = () => {
-  return (
-    <StLanding>
-      <Fade>
-        <Curriculum />
-      </Fade>
-    </StLanding>
-  );
+  return <></>;
 };
 
 export default CurriculumPage;
