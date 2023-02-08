32f09063b810b8564c53b1f9b332dbcc98cdf67f
soo <tnrud7844@ewhain.net>
2023-02-08 17:48:32 +0900

    경로가 이상해서 안 열리네요..

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,10 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "pages/login/Login";
 import Signup from "pages/sign/Signup";
 import Post from "pages/board/post";
+import PostMain from "pages/Post1/PostMain";
+import PostView from "pages/Post/PostView";
+import PostList from "pages/Post1/PostList";
+
 
 const App = () => {
   return (
@@ -9,6 +13,9 @@ const App = () => {
       <Route path="/login" element={<Login></Login>} />
       <Route path="/signup" element={<Signup></Signup>} />
       <Route path="/post" element={<Post></Post>} />
+      <Route path="/postview/:no" element={<PostView></PostView>} />
+      <Route path='/postmain' element={<PostMain></PostMain>}/>
+      <Route path='/postlist' element={<PostList></PostList>}/>
       <Route path="/" element={<Login></Login>} />
     </Routes>
   );
--- src/pages/board/post.jsx
+++ src/pages/board/post.jsx
@@ -1,39 +1,8 @@
 import styled from "styled-components";
 import './import.css';
 import { useState } from "react";
-import Axios from 'axios';
 
 function Post() {
-    const {movieContent, setMovieContent}=useState({
-        title: '',
-        content: ''
-      })
-      
-    const [viewContent, setViewContent] = useState([]);
-
-    useEffect(()=>{
-        Axios.get('http://localhost:3000/post/api/get').then((response)=>{
-          setViewContent(response.data);
-        })
-      },[viewContent])
-
-      const submitReview = ()=>{
-        Axios.post('http://localhost:8000/api/insert', {
-          title: movieContent.title,
-          content: movieContent.content
-        }).then(()=>{
-          alert('등록 완료!');
-        })
-      };
-      
-    const getValue=e=>{
-        const { name, value } = e.target;
-        setMovieContent({
-          ...movieContent,
-          [name]: value
-        })
-      };
-
   return (
     <div className="Post">
       <h1>게시판</h1>
