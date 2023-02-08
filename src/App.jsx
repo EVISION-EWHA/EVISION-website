da91853bce329ce1bf48accdbc1ba01ca6ba250a
jiwoorld <bncmdbev@gmail.com>
2023-02-08 20:21:17 +0900

    2023/02/08 수경님 변경상황까지 반영

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,14 @@ import Login from "pages/login/Login";
 import Signup from "pages/sign/Signup";
 import MainPage from "pages/Main/MainPage";
 import CurriculumPage from "pages/Curriculum/CurriculumPage";
+import Post from "pages/board/post";
+import PostMain from "pages/Post/PostMain";
+import PostView from "pages/Post/PostView";
+import PostList from "pages/Post/PostList";
+import UserList from "pages/userList/userList";
 import Header from "components/header";
-import Footer from "components/footer";\
+import Footer from "components/footer";
+
 const App = () => {
   return (
     <>
@@ -14,11 +20,14 @@ const App = () => {
         <Route path="/signup" element={<Signup></Signup>} />
         <Route path="/main" element={<MainPage></MainPage>} />
         <Route path="/curriculum" element={<CurriculumPage></CurriculumPage>} />
+        <Route path="/postview/:no" element={<PostView></PostView>} />
+        <Route path="/postmain" element={<PostMain></PostMain>} />
+        <Route path="/postlist" element={<PostList></PostList>} />
         <Route path="/admin" element={<UserList></UserList>} />
       </Routes>
       <Footer />
     </>
-     );
+  );
 };
 
 export default App;
