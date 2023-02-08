314f84a7de6f97e2b62e2e244619ef103e5a41e6
Jiwoo kwak <113344660+jiwoorld@users.noreply.github.com>
2023-02-08 20:07:06 +0900

    Revert "Soo"

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "pages/login/Login";
 import Signup from "pages/sign/Signup";
-import Post from "pages/board/post";
-import PostMain from "pages/Post1/PostMain";
-import PostView from "pages/Post/PostView";
-import PostList from "pages/Post1/PostList";
-
+import MainPage from "pages/Main/MainPage";
+import CurriculumPage from "pages/Curriculum/CurriculumPage";
+import Header from "components/header";
+import Footer from "components/footer";\
 const App = () => {
   return (
     <>
@@ -15,9 +14,6 @@ const App = () => {
         <Route path="/signup" element={<Signup></Signup>} />
         <Route path="/main" element={<MainPage></MainPage>} />
         <Route path="/curriculum" element={<CurriculumPage></CurriculumPage>} />
-        <Route path="/postview/:no" element={<PostView></PostView>} />
-        <Route path='/postmain' element={<PostMain></PostMain>}/>
-        <Route path='/postlist' element={<PostList></PostList>}/>
         <Route path="/admin" element={<UserList></UserList>} />
       </Routes>
       <Footer />
