80ae602628e2a5b70725688c11de859f17d95a51
jiwoorld <bncmdbev@gmail.com>
2023-02-14 17:27:22 +0900

    2023/02/14 login 서버 연동 코드 수정

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,35 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import Swal from "sweetalert2";
 import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 
 function Login({ setIsLogin }) {
+  const [inputId, setInputId] = useState('')
+  const [inputPw, setinputPw] = useState('')
+
+  const handleInputId = (e) => {
+    setInputId(e.target.value)
+  }
+  const handleInputPw = (e) => {
+    setinputPw(e.target.value)
+  }
+  const onClickLogin = () => {
+    console.log('click login')
+}
+
   const navigate = useNavigate();
+
   const onhandlePost = async (data) => {
     const { userId, userPw } = data;
     const postData = { userId, userPw };
-    postData.userId = data.id;
-    postData.userPw = data.password;
-
+    postData.userId = data.userId;
+    postData.userPw = data.userPw;
     await axios
-      .post("http://localhost:8080/login", postData)
+      .post('login', postData)
       .then((res) => {
+        console.log(postData);
         let submitBtn = document.getElementById("submit");
         submitBtn.addEventListener("click", function (e) {
           this.setAttribute("disabled", "true");
           this.setAttribute("disabledElevation", "true");
           this.setAttribute("disabledRipple", "true");
         });
-        navigate("/");
+        //navigate("/");
         setIsLogin(true);
       })
       .catch((err) => {
-        if (err.response.data === "userId") {
+        if (err.response === -2) {
           Swal.fire({
             width: 460,
             height: 260,
-            html: "<b> 로그인 실패</b><br><br>잘못된 이메일입니다",
+            html: "<b> 로그인 실패</b><br><br>존재하지 않는 아이디입니다",
             showConfirmButton: false,
             cancelButtonText: "확인",
             cancelButtonColor: "#CF5E53",
@@ -38,36 +52,52 @@ function Login({ setIsLogin }) {
             timer: 5000,
           });
           //존재하지 않는 이메일로 로그인 실패
-        } else if (err.response.data === "userPassword") {
-          Swal.fire({
-            width: 460,
-            height: 260,
-            html: "<b> 로그인 실패</b><br><br>잘못된 비밀번호입니다",
-            showConfirmButton: false,
-            cancelButtonText: "확인",
-            cancelButtonColor: "#CF5E53",
-            showCancelButton: true,
-            timer: 5000,
-          });
+        } 
+        else {
+          console.log("err")
         }
+        // else if (err.response.data === "userPassword") {
+        //   Swal.fire({
+        //     width: 460,
+        //     height: 260,
+        //     html: "<b> 로그인 실패</b><br><br>잘못된 비밀번호입니다",
+        //     showConfirmButton: false,
+        //     cancelButtonText: "확인",
+        //     cancelButtonColor: "#CF5E53",
+        //     showCancelButton: true,
+        //     timer: 5000,
+        //   });
+        // }
       });
   };
 
   const handleSubmit = (event) => {
     event.preventDefault();
+    console.log("click login")
     const data = new FormData(event.currentTarget);
     const loginData = {
-      id: data.get("id"),
-      password: data.get("password"),
+      userId: data.get("userId"),
+      userPw: data.get("userPw"),
     };
     onhandlePost(loginData);
   };
 
+  const hstyle = {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    width: "100%",
+    margin: "10rem 0 5rem 0",
+    padding: "5rem 0 5rem 0",
+    flexDirection: "column",
+    fontSize: "30px"
+  }
+
   return (
-    <StLogin onSubmit={handleSubmit}>
+    <form style={hstyle} onSubmit={handleSubmit}>
       <Stlabel>
         Id
-        <StInput type="text" id="id" name="id" autoComplete="id" />
+        <StInput type="text" id="userId" name="id" onChange={handleInputId} value={inputId} autoComplete="id" />
       </Stlabel>
       <Stlabel>
         Password
@@ -75,21 +105,23 @@ function Login({ setIsLogin }) {
           <StInput
             name="password"
             type="password"
-            id="password"
+            id="userPw"
+            value={inputPw}
+            onChange={handleInputPw}
             autoComplete="new-password"
           />
         </form>
       </Stlabel>
       <br />
-      <StLoginBtn type="submit" id="submit">
+      <StLoginBtn type="submit" id="submit" onClick={onClickLogin}>
         로그인
       </StLoginBtn>
-    </StLogin>
+    </form>
   );
 }
 export default Login;
 
-const StLogin = styled.div`
+const form = styled.form`
   display: flex;
   justify-content: center;
   align-items: center;
@@ -105,7 +137,7 @@ const Stlabel = styled.label`
   margin: 4rem 0 2rem 1rem;
   padding: 0 0 0 1rem;
   flex-direction: row;
-  font-size: 4rem;
+  font-size: 3rem;
 `;
 
 const StInput = styled.input`
--- src/pages/userList/userList.jsx
+++ src/pages/userList/userList.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 import "./userList.css";
+import axios from "axios";
+import Swal from "sweetalert2";
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
 import { DataGrid, GridRowsProp, GridColDef } from "@mui/x-data-grid";
 import InputLabel from "@mui/material/InputLabel";
 import MenuItem from "@mui/material/MenuItem";
@@ -35,6 +39,54 @@ function UserList() {
     { field: "col4", headerName: "승인 상태", width: 250 },
   ];
 
+  
+  // const onhandlePost = async (data) => {
+  //   const { userId, userPw } = data;
+  //   const postData = { userId, userPw };
+  //   postData.userId = data.userId;
+  //   postData.userPw = data.userPw;
+
+  //   await axios
+  //     .post("http://3.37.117.164:8080/login", postData)
+  //     .then((res) => {
+  //       let submitBtn = document.getElementById("submit");
+  //       submitBtn.addEventListener("click", function (e) {
+  //         this.setAttribute("disabled", "true");
+  //         this.setAttribute("disabledElevation", "true");
+  //         this.setAttribute("disabledRipple", "true");
+  //       });
+  //       //navigate("/");
+  //       //setIsLogin(true);
+  //     })
+  //     .catch((err) => {
+  //       if (err.response.data === "userId") {
+  //         Swal.fire({
+  //           width: 460,
+  //           height: 260,
+  //           html: "<b> 로그인 실패</b><br><br>잘못된 이메일입니다",
+  //           showConfirmButton: false,
+  //           cancelButtonText: "확인",
+  //           cancelButtonColor: "#CF5E53",
+  //           showCancelButton: true,
+  //           background: "#fff url(/image/swalBackground.png)",
+  //           timer: 5000,
+  //         });
+  //         //존재하지 않는 이메일로 로그인 실패
+  //       } else if (err.response.data === "userPassword") {
+  //         Swal.fire({
+  //           width: 460,
+  //           height: 260,
+  //           html: "<b> 로그인 실패</b><br><br>잘못된 비밀번호입니다",
+  //           showConfirmButton: false,
+  //           cancelButtonText: "확인",
+  //           cancelButtonColor: "#CF5E53",
+  //           showCancelButton: true,
+  //           timer: 5000,
+  //         });
+  //       }
+  //     });
+  // };
+
   return (
     <div
       className="userList"
@@ -42,7 +94,7 @@ function UserList() {
         height: 800,
         width: 1000,
         margin: "10em 18rem 22rem 65rem",
-        //display: "flex",
+        display: "flex",
         justifyContent: "center",
         alignItems: "center",
       }}
@@ -61,22 +113,6 @@ function UserList() {
         columns={columns}
         checkboxSelection
       />
-      {/* <Box className="" sx={{ minWidth: 120 }}>
-        <FormControl fullWidth>
-          <InputLabel id="demo-simple-select-label">Age</InputLabel>
-          <Select
-            labelId="demo-simple-select-label"
-            id="demo-simple-select"
-            value={age}
-            label="Age"
-            onChange={handleChange}
-          >
-            <MenuItem value={10}>Ten</MenuItem>
-            <MenuItem value={20}>Twenty</MenuItem>
-            <MenuItem value={30}>Thirty</MenuItem>
-          </Select>
-        </FormControl>
-      </Box> */}
     </div>
   );
 }
