09b3756e37d4cd8081c0cb83d525c1d0d8e6274f
soo <tnrud7844@ewhain.net>
2023-02-15 20:00:41 +0900

    새로 코드 작성, 서버 연결 미비

--- src/pages/board/post.jsx
+++ src/pages/board/post.jsx
@@ -1,19 +1,90 @@
-import styled from "styled-components";
-import './import.css';
-import { useState } from "react";
+import { useState, useEffect } from 'react';
+import './App.css';
+import { CKEditor } from '@ckeditor/ckeditor5-react';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import ReactHtmlParser from 'react-html-parser';
+// import ReactHtmlParser from 'react-html-parser';
+import Axios from 'axios';
 
 function Post() {
+  const [Content, setContent] = useState({
+    title: '',
+    content: ''
+  })
+
+  const [viewContent, setViewContent] = useState([]);
+
+  useEffect(()=>{
+    Axios.get('http://localhost:8000/api/get').then((response)=>{
+      setViewContent(response.data);
+    })
+  },[viewContent])
+
+  const submitReview = ()=>{
+    Axios.post('http://localhost:8000/api/inssert', {
+      title: Content.title,
+      content: Content.content
+    }).then(()=>{
+      alert('등록 완료!');
+    })
+  };
+
+  const getValue = e => {
+    const { name, value } = e.target;
+    setContent({
+      ...Content,
+      [name]: value
+    })
+  };
+
+
   return (
     <div className="Post">
       <h1>게시판</h1>
-      <div className='form-wrapper'>
-        <h3>제목
-        <input className="title-input" type='text'/>
-        </h3>
-        <h4>내용</h4>
-          <textarea className="text-area" type='text'></textarea>
+      <div className='container'>
+        {viewContent.map(element =>
+          <div style={{ border: '1px solid #333' }}>
+            <h2>{element.title}</h2>
+            <div>
+              {ReactHtmlParser(element.content)}
+            </div>
+          </div>
+        )}
       </div>
-      <button className="submit-button">작성하기</button>
+      <div className='form-wrapper'>
+        <input className="title-input"
+          type='text'
+          placeholder='제목'
+          onChange={getValue}
+          name='title'
+        />
+        <CKEditor
+          editor={ClassicEditor}
+          data="<p>내용을 입력하세요.</p>"
+          onReady={editor => {
+          
+            console.log('Editor is ready to use!', editor);
+          }}
+          onChange={(event, editor) => {
+            const data = editor.getData();
+            console.log({ event, editor, data });
+            setContent({
+              ...Content,
+              content: data
+            })
+          }}
+          onBlur={(event, editor) => {
+            console.log('Blur.', editor);
+          }}
+          onFocus={(event, editor) => {
+            console.log('Focus.', editor);
+          }}
+        />
+      </div>
+      <button
+        className="submit-button"
+        onClick={submitReview}
+        >입력</button>
     </div>
   );
 }
