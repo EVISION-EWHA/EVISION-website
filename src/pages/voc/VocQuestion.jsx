b7b9480b07b0c0c81717c13a1e00c88384a9f912
soo <tnrud7844@ewhain.net>
2023-02-21 16:08:58 +0900

    수정필요

--- src/pages/voc/VocQuestion.jsx
+++ src/pages/voc/VocQuestion.jsx
@@ -1,68 +1,91 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import Input from "@mui/material/Input";
 import { API } from "../../config";
 import './VocView.css';
 
-function GetCategory() {
-  const [category, setCategory] = useState({});
+function VocQuestion() {
+    const [inputContent, setInputContent] = useState("");
+
+    const handleInputContent = (e) => {
+        setInputContent(e.target.value);
+      };
+
+    const onClickPosting= () => {
+        console.log("click post");
+      };
+
+const [allData, setAllData] = React.useState({});
+
+const onhandlePost = async (data) => {
+    const { content, writerId } = data;
+    const postData = { content, writerId };
+    postData.content = data.content;
+    postData.writerId = data.writerId;
+    try {
+      await axios
+        .post(`${API.Board}`, postData)
+        .then((res) => {
+          console.log(res);
+          let submitBtn = document.getElementById("submit");
+          submitBtn.addEventListener("click", function (e) {
+            this.setAttribute("disabled", "true");
+            this.setAttribute("disabledElevation", "true");
+            this.setAttribute("disabledRipple", "true");
+          });
+          const status = res.data;
+          console.log(status);
+        });
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   useEffect(() => {
-    axios.post(`${API.Board}`).then((response)=> {
-      setCategory(response.data);
-      alert("등록됐습니다.");
-    })
+    if (localStorage.getItem("userId") === null) {
+      window.location.replace("http://localhost:3000");
+    }
   }, []);
 
-  const categories = (Object.values(category)).map((item) => (
-    <option key={item.id} value={item.id}>
-      {item.displayName}
-    </option>
-  ));
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("click post");
+    const loginData = {
+      content: inputContent,
+      writerId: localStorage.getItem("userId"),
+    };
+    console.log(loginData)
+    onhandlePost(loginData);
+  };
 
-  return categories;
-}
-
-const HandleQuestionSubmit = async({body}) => {
-  const headers = {
-    'Content-Type' : 'application/json',
-    'Authorization' : "Bearer cognito 의 access token"
-  }
-
-  const response = await axios.post(`${API.Board}`, body, {headers: headers}).then((response) => {
-    console.log('status : '+response.status);
-  }).catch((error) => {
-    console.log('error : '+error);
-  });
-}
-
-function VocQuestion() {
-  const categories = GetCategory();
-  const [title, setTitle] = useState('');
-  const [content, setContent] = useState('');
-  const [stationId, setStationId] = useState('ST-4');
-
-  const body = {
-      title: title,
-      content: content,
-      stationId: stationId
-    }
-
-  return (<>
+  return (
+    <form onSubmit={handleSubmit}>
     <div className="title">
         <h2>게시글 작성</h2>
     </div>
     <div className="voc-view-wrapper">
         <div className="voc-view-row">
-            <label>제목</label>
-            <input onChange={(event) => setTitle(event.target.value)}></input>
+            {/* <label>제목</label>
+            <input onChange={(event) => setTitle(event.target.value)}></input> */}
         </div>
         <div className="voc-view-row">
             <label>내용</label>
-            <textarea onChange={(event) => setContent(event.target.value)}></textarea>
+            <Input
+          name="content"
+          id="content"
+          sx={{
+            background: "transparent",
+            color: "white",
+            width: "40rem",
+            fontSize: "3rem",
+          }}
+          value={inputContent}
+          onChange={handleInputContent}
+        />
         </div>
-    <button className="voc-view-go-list-btn" onClick={() => HandleQuestionSubmit({body})}>등록</button>
+    <button type="submit" id="submit" onClick={onClickPosting}>등록</button>
     </div>
-    </>);
+    </form>
+  );
 }
-  
 export default VocQuestion;
