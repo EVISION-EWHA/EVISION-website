17d705149fe3695daf7870b2c4af2e4b3517bc88
jiwoorld <bncmdbev@gmail.com>
2023-02-23 15:52:18 +0900

    admin board css 수정

--- src/pages/Admin/Admin.jsx
+++ src/pages/Admin/Admin.jsx
@@ -91,7 +91,7 @@ function Admin({ names, grade }) {
     display: "flex",
     justifyContent: "center",
     margin: "auto",
-    padding: "5rem 40rem 5rem 40rem",
+    padding: "5rem 40rem 5rem 45rem",
     flexDirection: "column",
     fontSize: "30px",
     backgroundColor: "black",
